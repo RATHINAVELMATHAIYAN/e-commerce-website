61c4e2791ecb6a813d11008bfaef622ae9967dfe
RATHINAVELMATHAIYAN <smr.rathinavel@gmail.com>
2024-08-06 02:25:45 +0530

    added all e-commerce files

---
+++ collection.js
@@ -0,0 +1,20 @@
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener{
+    "keyup", function () {
+        var enteredValue = event.target.value.toUpperCase()
+
+        for (count = 0; count < productlist.length; count = count + 1) {
+            var productname = productlist[count].querySelector("p").textContent
+            if (productname.toUpperCase().indexOf(enteredValue0) < 0) {
+                productlist[count].style.display = "none"
+            }
+            else {
+                productlist[count].style.display = "block"
+            }
+        }
+
+    }
+}
